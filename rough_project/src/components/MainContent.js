111a863b9406b3254e9ea19ade8f71447a905500
Vaibhav <haibatpure.vaibhav@gmail.com>
2024-07-11 23:01:27 +0530

    Vision and Mission
    Added Vision and Mission Page

--- rough_project/src/components/MainContent.js
+++ rough_project/src/components/MainContent.js
@@ -2,12 +2,14 @@ import React from 'react'
 import CarouselComponent from './CarouselComponent'
 import AboutPlatform from './AboutPlatform'
 import LoginPage from './LoginPage'
+import VisionMission from './VisionMission'
 
 export default function MainContent() {
     return (
         <div>
             <CarouselComponent></CarouselComponent>
             <AboutPlatform></AboutPlatform>
+            <VisionMission></VisionMission>
             <LoginPage></LoginPage>
         </div>
     )
---
+++ rough_project/src/components/VisionMission.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const VisionMission = () => {
+  return (
+    <div id="VisionMission" className="container" style={{ backgroundColor: 'rgb(194, 237, 248)', borderRadius: '5%', margin: '2% auto', padding: '5%', maxWidth: '90%' }}>
+      <h1 className="text-center">Vision and Mission</h1>
+      <p>
+        <br/>
+        <b>Our Vision:</b> To improve education by using technology to make learning easier and more engaging.
+      </p>
+      <p>
+      <br/>
+        <b>Our Mission:</b> To create a platform that supports academic success and helps students, parents, and teachers work together.
+      </p>
+    </div>
+  );
+}
+
+export default VisionMission;
